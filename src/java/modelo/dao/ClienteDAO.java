b2a87797fcc1674e411225f000c54e9487ce842d
mbentor <mbentorupo@gmail.com>
2025-05-19 13:40:21 +0200

    Creación clienteDAO
    Se han implementado los métodos DAO de cliente

---
+++ src/java/modelo/dao/ClienteDAO.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import java.util.List;
+import modelo.Cliente;
+import modelo.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author marin
+ */
+public class ClienteDAO {
+    
+    Session session = null;
+
+    public ClienteDAO() {
+    }
+    
+    public void altaCliente(Cliente c) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.save(c);
+        tx.commit();
+    }
+    
+    public void bajaCliente(Cliente c) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.delete(c);
+        tx.commit();
+    }
+
+    public void actualizarCliente(Cliente c) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.update(c);
+        tx.commit();
+    }
+    
+    public Cliente consultarCliente(int idCliente) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cliente WHERE idCliente = " + idCliente);
+        Cliente c = (Cliente) q.uniqueResult();
+        tx.commit();
+        return c;
+    }
+
+    public List<Cliente> buscarCliente(String termino) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("FROM Cliente WHERE dni LIKE :termino OR telefono LIKE :termino");
+        q.setParameter("termino", "%" + termino + "%");
+        List<Cliente> lista = q.list();
+        tx.commit();
+        return lista;
+    }
+    
+}
